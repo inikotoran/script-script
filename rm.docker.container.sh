ec4a5de6e95e9f5c8290fcb22711bf7388119576
niko.juliyanto <niko.juliyanto@go-jek.com>
2020-10-11 14:45:05 +0700

    fix docker rm container

--- rm.docker.container.sh
+++ rm.docker.container.sh
@@ -1,6 +1,6 @@
 docker container ls -a | grep $1 > will_be_removed.txt;
 while read p; do
-	docker container rm $(echo \"$p\" | awk '{print $1;}')
+	docker container rm $(echo $p | awk '{print $1;}')
 done <will_be_removed.txt;
 rm will_be_removed.txt;
 
